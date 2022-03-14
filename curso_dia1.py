0505857870590bc3d37b6a4d8f19d36b2aac4e63
Diego Galeano <diegodag.24@gmail.com>
2022-03-14 13:30:52 -0500

    tipos de datos v1

---
+++ curso_dia1.py
@@ -0,0 +1,34 @@
+#concatenación (+) unir dos o mas cadenas de texto, suma de numeros o texto 
+#se debe usar str() el numero
+
+cumple_uno = "Yo tengo "
+edad = 35
+cumple_dos = " años"
+
+Mensaje_completo = cumple_uno + str(edad) + cumple_dos
+
+print(Mensaje_completo)
+
+# mas igual
+
+"""numero_Km = 5
+numero_Km = numero_Km + 4
+print(numero_Km)"""
+
+numero_Km = 5
+numero_Km += 4
+print(numero_Km)
+
+"""nombre = "Diego "
+apellido = "Galeano"
+nombre_completo = nombre + apellido
+print(nombre_completo)"""
+
+nombre = "Diego "
+nombre += "Armando"
+print(nombre)
+
+
+
+
+
