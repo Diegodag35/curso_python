663d3abf6c9b49be7f5ae75c3f6b7690272e39d1
Diego Galeano <diegodag.24@gmail.com>
2022-03-16 18:42:59 -0500

    agregando programa raiz cuadrada y clase de variables

--- raiz_cuadrada.py
+++ raiz_cuadrada.py
@@ -1,8 +1,14 @@
 #Este programa calcula la raiz cuadrada de un numero 
-
 from unicodedata import numeric
-
-
+##################################################################
+#primera forma para hallar la raiz cuadrada de un numero pedido 
+##################################################################
 numero = int (input("La raiz cuadrada de:"))
 raiz_cuadrada = numero **(1/2)
 print(raiz_cuadrada)
+
+##################################################################
+#Segunda forma para hallar la raiz cuadrada de un numero pedido
+# usando la funcion sqrt 
+##################################################################
+
---
+++ variables.py
@@ -0,0 +1,7 @@
+# VARIABLES
+###############################################################
+#definiciones
+#Es una cantidad suseptible de tomar distintos valores numéricos dentro de un conjunto de números especificados
+# Es una caja en la cual puedo guardar objetos.
+###############################################################
+
