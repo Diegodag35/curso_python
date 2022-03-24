fe21a0fb94ba2d7d0ff67a22a539f802d182ce22
Diegodag35 <95539119+Diegodag35@users.noreply.github.com>
2022-03-24 12:56:14 -0500

    Delete ejercicio_1.py

--- ejercicio_1.py
+++
@@ -1,4 +0,0 @@
-# Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.
-print("Buenas noches, imprima una frase de por qué te gusta la programación ")
-frase = input()
-print(frase)
