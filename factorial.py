a0b0228a480ff42661ea861aea10c4b74fbdd3f0
diegodag <diegodag.24@gmail.com>
2022-03-24 13:06:13 -0500

    agregar comentarios a las lineas

--- factorial.py
+++ factorial.py
@@ -4,14 +4,12 @@ def run():
 
     #factorial de un numero
 
-    numero = int(input("ingrese el numero "))
-    factorial = 1 
-
-    for contador in range(1,(numero + 1)):
-        factorial = factorial*contador
-        contador += 1
-
-    print(str(numero) + "! = ", factorial)
+    numero = int(input("ingrese el numero ")) #el usuario ingresa el numero al cual desea sacar su factorial
+    factorial = 1 #inicializamos factorial para evitar el prducto por cero
+    for contador in range(1,(numero + 1)): #el segundo parametro le sumo 1 para que tome el valor de numnero incluido.
+        factorial = factorial*contador  #operacion para guardar los productos
+        contador += 1 #aumento de 1 por cada ciclo
+    print(str(numero) + "! = ", factorial) 
     
 
 
